2430a3c1770d73e9232a28494cece48cda090b5c
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2024-11-28 20:24:39 +0900

    LEDチカチカ

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 // /Users/<username>/.platformio/packages/framework-arduinoespressif32/cores/esp32/Arduino.h
 #include <Arduino.h>
 // define led according to pin diagram in article
-const int led = D10;  // there is no LED_BUILTIN available for the XIAO ESP32C3.
+const int led = D0;  // there is no LED_BUILTIN available for the XIAO ESP32C3.
 
 void setup() {
     // initialize digital pin led as an output
@@ -13,4 +13,12 @@ void loop() {
     delay(1000);              // wait for a second
     digitalWrite(led, LOW);   // turn the LED off
     delay(1000);              // wait for a second
+}
+
+int main() {
+    setup();
+    while (true) {
+        loop();
+    }
+    return 0;
 }
