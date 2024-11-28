8732ac7de44a8913dd7941a5b958d76b5a8d3a81
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2024-11-28 21:20:06 +0900

    LEDの明るさをフェードする

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 // /Users/<username>/.platformio/packages/framework-arduinoespressif32/cores/esp32/Arduino.h
 #include <Arduino.h>
 // define led according to pin diagram in article
-const int led = D0;
-const int button = D1;
+const uint8_t led = D0;
+const uint8_t button = D1;
 bool buttonState = false;
+uint8_t brightness = 0;
+int16_t fadeAmount = 5;
 
 void setup() {
     pinMode(led, OUTPUT);
@@ -13,6 +15,21 @@ void setup() {
 }
 
 void loop() {
+    // set the brightness of pin 9:
+    analogWrite(led, brightness);
+
+    // change the brightness for next time through the loop:
+    brightness = brightness + fadeAmount;
+
+    // reverse the direction of the fading at the ends of the fade:
+    if (brightness <= 0 || brightness >= 255) {
+        fadeAmount = -fadeAmount;
+    }
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+}
+
+void tmp() {
     digitalWrite(led, LOW);
     Serial.printf("buttonState: %d\n", buttonState);
     if (buttonState = !digitalRead(button); buttonState) {
