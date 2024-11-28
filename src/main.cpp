cf80d189e2de143ae4797cf6c530ca0ab4431b62
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2024-11-28 20:25:23 +0900

    ボタンを押したらSerialPrint

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 // /Users/<username>/.platformio/packages/framework-arduinoespressif32/cores/esp32/Arduino.h
 #include <Arduino.h>
 // define led according to pin diagram in article
-const int led = D0;  // there is no LED_BUILTIN available for the XIAO ESP32C3.
+const int led = D0;
+const int button = D1;
+bool buttonState = false;
 
 void setup() {
-    // initialize digital pin led as an output
     pinMode(led, OUTPUT);
+
+    Serial.begin(115200);
+    Serial.println("Hello World");
+    pinMode(button, INPUT_PULLUP);
 }
 
 void loop() {
-    digitalWrite(led, HIGH);  // turn the LED on
-    delay(1000);              // wait for a second
-    digitalWrite(led, LOW);   // turn the LED off
-    delay(1000);              // wait for a second
+    // digitalWrite(led, HIGH);  // turn the LED on
+    // delay(1000);              // wait for a second
+    // digitalWrite(led, LOW);   // turn the LED off
+    // delay(1000);              // wait for a second
+
+    buttonState = !digitalRead(button);
+    Serial.printf("buttonState: %d\n", buttonState);
+    delay(1000);
 }
 
 int main() {
