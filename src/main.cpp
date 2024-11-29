1c3594fd6aba4e4f863164a3d46d3836007703fa
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2024-11-30 01:33:53 +0900

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,15 @@ void setup() {
     Serial.begin(115200);
     board.led().set_brightness(150);
     std::vector<bool> pattern = {1, 0, 1, 0, 1, 1, 0, 1};
-    for (auto &&led : pattern) {
-        if (led) {
+    for (auto &&turn_on : pattern) {
+        if (turn_on) {
             board.led().on();
             Serial.printf("LED ON\n");
-            delay(1000);
         } else {
             board.led().off();
             Serial.printf("LED OFF\n");
-            delay(1000);
         }
+        delay(1000);
     }
     board.led().off();
 }
